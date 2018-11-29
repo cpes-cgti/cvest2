f031b8f0939386fb5e1c4c3ef8840427697151fa
IFPE-PSQ-Jobsontn <jobson.nascimento@pesqueira.ifpe.edu.br>
2018-11-29 00:48:10 -0200

    Atualizado em 29/11/2018 00:48.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Redaction;
+use App\Models\Corrector;
+use App\User;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +27,16 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $redactions = DB::table('redactions')
+                   ->select('status', DB::raw('COUNT(id) as qtde'))
+                   ->groupBy('status')->get();
+        
+        $colors = collect();
+        $colors->put('Digitalizada', '#d2d6de');
+        $colors->put('Para correção', '#f39c12');
+        $colors->put('Corrigida (1x)', '#3c8dbc');
+        $colors->put('Corrigida (concluído)', '#00a65a');
+        $colors->put('Inconsistência', '#f56954');
+        return view('home', compact('redactions', 'colors'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,31 @@
 
 @section('content')
     <div class="row">
+        <div class="col-md-6">
+            <div class="box box-danger">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Redações</h3>
+                </div>
+                <div class="box-body">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <canvas id="pieChart" style="height: 187px; width: 374px;" width="467" height="233"></canvas>        
+                        </div>
+                        <div class="col-md-4">
+                            <ul class="chart-legend clearfix">
+                                <li><i class="fas fa-square text-gray"></i> Digitalizada</li>
+                                <li><i class="fas fa-square text-yellow"></i> Para correção</li>
+                                <li><i class="fas fa-square text-blue"></i> Corrigida (1x)</li>
+                                <li><i class="fas fa-square text-green"></i> Corrigida (concluído)</li>
+                                <li><i class="fas fa-square text-red"></i> Inconsistência</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
         <div class="col-lg-3 col-xs-6">
             <div class="small-box bg-blue">
                 <div class="inner">
@@ -57,4 +82,55 @@
             </div>
         </div>
     </div>
+@stop
+
+@section('js')
+    <script src="{{ asset('js/Chart.js') }}"></script>
+    <script>
+        $(function () {
+            //-------------
+            //- PIE CHART -
+            //-------------
+            // Get context with jQuery - using jQuery's .get() method.
+            var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
+            var pieChart       = new Chart(pieChartCanvas)
+            var PieData        = [
+            @foreach ($redactions as $r)    
+                {
+                    value    : '{{ $r->qtde }}',
+                    color    : '{{ $colors[$r->status] }}',
+                    highlight: '{{ $colors[$r->status] }}',
+                    label    : '{{ $r->status }}'
+                },
+            @endforeach
+            ]
+            var pieOptions     = {
+            //Boolean - Whether we should show a stroke on each segment
+            segmentShowStroke    : true,
+            //String - The colour of each segment stroke
+            segmentStrokeColor   : '#fff',
+            //Number - The width of each segment stroke
+            segmentStrokeWidth   : 2,
+            //Number - The percentage of the chart that we cut out of the middle
+            percentageInnerCutout: 50, // This is 0 for Pie charts
+            //Number - Amount of animation steps
+            animationSteps       : 100,
+            //String - Animation easing effect
+            animationEasing      : 'easeOutBounce',
+            //Boolean - Whether we animate the rotation of the Doughnut
+            animateRotate        : true,
+            //Boolean - Whether we animate scaling the Doughnut from the centre
+            animateScale         : false,
+            //Boolean - whether to make the chart responsive to window resizing
+            responsive           : true,
+            // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+            maintainAspectRatio  : true,
+            //String - A legend template
+            legendTemplate       : '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<segments.length; i++){%><li><span style="background-color:<%=segments[i].fillColor%>"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>'
+            }
+            //Create pie or douhnut chart
+            // You can switch between pie and douhnut using the method below.
+            pieChart.Doughnut(PieData, pieOptions)
+        })
+    </script>
 @stop
